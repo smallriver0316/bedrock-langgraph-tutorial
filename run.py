b149ef1148e2264a8df03e5b9d14a655a6fe068f
smallriver0316 <stream0316@gmail.com>
2024-10-17 06:55:46 +0900

    Replaced BasicToolNode with ToolNode prebuilt on LangGraph

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from langchain_aws import ChatBedrockConverse
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import ToolMessage
 from typing_extensions import TypedDict
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -15,28 +16,28 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-class BasicToolNode:
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
+# class BasicToolNode:
+#     def __init__(self, tools: list) -> None:
+#         self.tools_by_name = {tool.name: tool for tool in tools}
 
-    def __call__(self, inputs: list) -> None:
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
+#     def __call__(self, inputs: list) -> None:
+#         if messages := inputs.get("messages", []):
+#             message = messages[-1]
+#         else:
+#             raise ValueError("No message found in input")
 
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"])
-            outputs.append(
-                ToolMessage(
-                  content=json.dumps(tool_result),
-                  name=tool_call["name"],
-                  tool_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
+#         outputs = []
+#         for tool_call in message.tool_calls:
+#             tool_result = self.tools_by_name[tool_call["name"]].invoke(
+#                 tool_call["args"])
+#             outputs.append(
+#                 ToolMessage(
+#                   content=json.dumps(tool_result),
+#                   name=tool_call["name"],
+#                   tool_call_id=tool_call["id"],
+#                 )
+#             )
+#         return {"messages": outputs}
 
 
 graph_builder = StateGraph(State)
@@ -51,34 +52,39 @@ def chatbot(state: State):
     return {"messages": [model.invoke(state["messages"])]}
 
 
-def route_tools(state: State):
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(
-            f"No messages found in input state to tool_edge: {
-                state
-            }")
+# def route_tools(state: State):
+#     if isinstance(state, list):
+#         ai_message = state[-1]
+#     elif messages := state.get("messages", []):
+#         ai_message = messages[-1]
+#     else:
+#         raise ValueError(
+#             f"No messages found in input state to tool_edge: {
+#                 state
+#             }")
 
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
+#     if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+#         return "tools"
 
-    return END
+#     return END
 
 
-tool_node = BasicToolNode(tools=[tool])
+# tool_node = BasicToolNode(tools=[tool])
+tool_node = ToolNode(tools=[tool])
 
 # Add nodes
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("tools", tool_node)
 # Add edges
 graph_builder.add_edge(START, "chatbot")
+# graph_builder.add_conditional_edges(
+#     "chatbot",
+#     route_tools,
+#     {"tools": "tools", END: END}
+# )
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    {"tools": "tools", END: END}
+    tools_condition,
 )
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge("chatbot", END)
