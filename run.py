43e7f78ef877935cd8ecfb9b616c89322d2774ab
smallriver0316 <stream0316@gmail.com>
2024-10-18 07:02:11 +0900

    Added memory to graph

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from langchain_aws import ChatBedrockConverse
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 from typing_extensions import TypedDict
 from langchain_community.tools.tavily_search import TavilySearchResults
 
@@ -14,19 +15,21 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-graph_builder = StateGraph(State)
-
-tool = TavilySearchResults(max_results=2)
-tools = [tool]
-model = ChatBedrockConverse(
-    model="anthropic.claude-3-haiku-20240307-v1:0").bind_tools(tools)
-
-
 def chatbot(state: State):
     return {"messages": [model.invoke(state["messages"])]}
 
 
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
 tool_node = ToolNode(tools=[tool])
+
+model = ChatBedrockConverse(
+    model="anthropic.claude-3-haiku-20240307-v1:0").bind_tools(tools)
+
+memory = MemorySaver()
+
+# Construct graph
+graph_builder = StateGraph(State)
 
 # Add nodes
 graph_builder.add_node("chatbot", chatbot)
@@ -40,11 +43,13 @@ graph_builder.add_conditional_edges(
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge("chatbot", END)
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
 
 
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
+    for event in graph.stream({"messages": [("user", user_input)]}, config):
         for value in event.values():
             print("Assistant", value["messages"][-1].content)
 
